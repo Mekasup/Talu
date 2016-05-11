cdbd4e71ffd6b89c52ee026a9a996fca78c20969
Mekasup <bastih@hotmail.fr>
2016-05-11 14:57:06 +0200

    Mise en place patern MVC

---
+++ Talu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Talu.Startup))]
+namespace Talu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
